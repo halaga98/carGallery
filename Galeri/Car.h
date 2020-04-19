af9fbe293568c384b0b53e4b490410bcf681760d
hüseyi kıldacı <huso_kildaci@hotmail.com>
2020-04-19 15:36:54 +0300

    düzeltme

--- Galeri/Car.h
+++ Galeri/Car.h
@@ -1,6 +1,26 @@
+#include <iostream>
+using namespace std;
 class Car {
 
 	public:
+		string Brand;
+		int model_Year;
+		int car_Km;
+		int car_Price;
+		int car_Id;
+		
+		Car();
+		void Setmodel_Year();
+		void Setcar_Km();
+		void Setcar_Price();
+		void Setcar_Id();
+		bool operator == (const MyItem&);
+		friend ostream& operator <<
+			(ostream&, const MyItem&);
+
+
+		
+		
 
 
 
